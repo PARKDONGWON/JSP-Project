9560aace321e28e4a6c5fed932e7a333e2b4258f
PARKDONGWON <qkrehddnjs05@naver.com>
2018-01-09 16:19:44 +0900

    2. 자유게시판 DAO,DTO 구성
    자유게시판에 해당하는
    DAO(게시판,댓글)
    DTO(게시판,댓글)
    기본틀 작성.

---
+++ ProjectCafeTalk/src/free/cafe/dao/FreeCafeDao.java
@@ -0,0 +1,166 @@
+package free.cafe.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import free.cafe.dto.FreeCafeDto;
+import free.cafe.mybatis.SqlMapConfig;
+
+
+
+public class FreeCafeDao {
+	private static FreeCafeDao dao;
+	private static SqlSessionFactory factory;
+	private FreeCafeDao(){}
+	public static FreeCafeDao getInstance(){
+		if(dao==null){
+			dao=new FreeCafeDao();
+			factory=SqlMapConfig.getSqlSession();
+		}
+		return dao;
+	}
+	
+	
+	//전체 글의 갯수를 리턴하는 메소드
+	public int getCount(FreeCafeDto dto) {
+		SqlSession session=null;
+		int count=0;
+		try {
+			session=factory.openSession();
+			/*
+			 *  검색 조건에 맞는 row 의 갯수만 셀수 있도록 
+			 *  parameter 를 CafeDto 를 전달한다.
+			 *  parameterType : CafeDto
+			 */
+			count=session.selectOne("cafe.getCount", dto);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			session.close();
+		}
+		return count;
+	}
+	
+	//글정보를 수정하는 메소드
+	public void update(FreeCafeDto dto) {
+		SqlSession session=null;
+		try {
+			session=factory.openSession(true);
+			session.update("cafe.update", dto);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			session.close();
+		}
+	}
+	
+	//글정보를 삭제하는 메소드
+	public void delete(int num) {
+		SqlSession session=null;
+		try {
+			session=factory.openSession(true);
+			session.delete("freecafe.delete", num);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			session.close();
+		}
+	}
+	
+	//글정보를 리턴해주는 메소드
+	public FreeCafeDto getData(FreeCafeDto dto) {
+		SqlSession session=null;
+		FreeCafeDto resultDto=null;
+		try {
+			session=factory.openSession();
+			/*
+			 *  Mapper namespace : cafe
+			 *  sql id : getData
+			 *  parameterType : CafeDto
+			 *  resultType : CafeDto
+			 */
+			resultDto=session.selectOne("cafe.getData", dto);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			session.close();
+		}
+		return resultDto;
+	}
+	
+	//조회수를 1 증가 시키는 메소드
+	public void increaseViewCount(int num) {
+		SqlSession session=null;
+		try {
+			session=factory.openSession(true);
+			session.update("cafe.increaseViewCount", num);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			session.close();
+		}	
+	}
+	
+	
+	//글목록을 리턴해주는 메소드
+	/*
+	 *  인자로 전달된 CafeDto 에는 startRowNum 과 endRowNum 이 
+	 *  들어 있다. 이값을 이용해서 SELECT 한다. 
+	 */
+	public List<FreeCafeDto> getList(FreeCafeDto dto){
+		SqlSession session=null;
+		List<FreeCafeDto> list=null;
+		try {
+			session=factory.openSession();
+			/*
+			 *  Mapper 의 namespace : cafe
+			 *  sql id : getList
+			 *  parameterType : CafeDto
+			 *  resultType : CafeDto
+			 */
+			list=session.selectList("cafe.getList", dto);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			session.close();
+		}
+		return list;
+	}
+	//글정보를 저장하는 메소드 
+	public void insert(FreeCafeDto dto) {
+		SqlSession session=null;
+		try {
+			session=factory.openSession(true);
+			session.insert("freecafe.insert", dto);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			session.close();
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
